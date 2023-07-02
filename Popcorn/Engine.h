cf01413ad353f85b51273e099ada1435ef632d30
WonVoyage <137709423+WonVoyage@users.noreply.github.com>
2023-07-02 16:52:43 +0300

    Add input from keybord, implemented redraw platform

---
+++ Popcorn/Engine.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <Windows.h>
+
+enum EKey_Type
+{
+	EKT_Left,
+	EKT_Right,
+	EKT_Space
+};
+
+void Init_Engine(HWND hwnd);
+void Draw_Frame(HDC hdc, RECT &paint_area);
+int On_Key_Down(EKey_Type key_type);
