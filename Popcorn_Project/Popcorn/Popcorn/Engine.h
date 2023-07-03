59f3f2eb1d5ba02aabb95d28113e40f3eb044501
WonVoyage <137709423+WonVoyage@users.noreply.github.com>
2023-07-03 18:50:23 +0300

    Move level to level class

--- Popcorn_Project/Popcorn/Popcorn/Engine.h
+++ Popcorn_Project/Popcorn/Popcorn/Engine.h
@@ -30,13 +30,14 @@ enum EKey_Type
 const int Timer_ID = WM_USER + 1;
 //------------------------------------------------------------------------------------------------------------
 class AsEngine;
+class ALevel;
 class ABall
 {
 public:
 	ABall();
 
 	void Draw(HDC hdc, RECT &paint_area, AsEngine *engine);
-	void Move(AsEngine *engine);
+	void Move(AsEngine *engine, ALevel *level);
 
 	HPEN Ball_Pen;
 	HBRUSH Ball_Brush;
@@ -51,6 +52,33 @@ private:
 	RECT Ball_Rect, Prev_Ball_Rect;
 };
 //------------------------------------------------------------------------------------------------------------
+class ALevel
+{
+public:
+	void Init();
+	void Check_Level_Brick_Hit(int &next_y_pos, double &ball_direction);
+	void Draw_Level(HDC hdc, RECT &paint_area);
+
+	static const int Level_Width = 12;  // Ширина уровня в ячейках
+	static const int Level_Height = 14;  // Высота уровня в ячейках
+	static const int Level_X_Offset = 8;
+	static const int Level_Y_Offset = 6;
+	static const int Cell_Width = 16;
+	static const int Cell_Height = 8;
+
+private:
+	void Draw_Brick(HDC hdc, int x, int y, EBrick_Type brick_type);
+	void Set_Brick_Letter_Colors(bool is_switch_color, HPEN &front_pen, HBRUSH &front_brush, HPEN &back_pen, HBRUSH &back_brush);
+	void Draw_Brick_Letter(HDC hdc, int x, int y, EBrick_Type brick_type, ELetter_Type letter_type, int rotation_step);
+
+	HPEN Brick_Red_Pen, Brick_Blue_Pen, Letter_Pen;
+	HBRUSH Brick_Red_Brush, Brick_Blue_Brush;
+	RECT Level_Rect;
+
+	static const int Brick_Width = 15;
+	static const int Brick_Height = 7;
+};
+//------------------------------------------------------------------------------------------------------------
 class AsEngine
 {
 public:
@@ -60,7 +88,8 @@ public:
 	void Draw_Frame(HDC hdc, RECT &paint_area);
 	int On_Key_Down(EKey_Type key_type);
 	int On_Timer();
-	void Check_Level_Brick_Hit(int &next_y_pos);
+
+	static void Create_Pen_Brush(unsigned char r, unsigned char g, unsigned char b, HPEN &pen, HBRUSH &brush);
 
 	HWND Hwnd;
 	HPEN BG_Pen;
@@ -69,42 +98,29 @@ public:
 	int Platform_Width;
 
 	static const int Global_Scale = 3;
-	static const int Level_Width = 12;  // Ширина уровня в ячейках
-	static const int Level_Height = 14;  // Высота уровня в ячейках
-	static const int Level_X_Offset = 8;
-	static const int Level_Y_Offset = 6;
-	static const int Cell_Width = 16;
-	static const int Cell_Height = 8;
-	static const int Max_X_Pos = Level_X_Offset + Cell_Width * Level_Width;
+	static const int Max_X_Pos = ALevel::Level_X_Offset + ALevel::Cell_Width * ALevel::Level_Width;
 	static const int Max_Y_Pos = 199 - ABall::Ball_Size;
 	static const int Platform_Y_Pos = 185;
 	static const int Border_X_Offset = 6;
 	static const int Border_Y_Offset = 4;
 
 private:
-	void Create_Pen_Brush(unsigned char r, unsigned char g, unsigned char b, HPEN &pen, HBRUSH &brush);
 	void Redraw_Platform();
-	void Draw_Brick(HDC hdc, int x, int y, EBrick_Type brick_type);
-	void Set_Brick_Letter_Colors(bool is_switch_color, HPEN &front_pen, HBRUSH &front_brush, HPEN &back_pen, HBRUSH &back_brush);
-	void Draw_Brick_Letter(HDC hdc, int x, int y, EBrick_Type brick_type, ELetter_Type letter_type, int rotation_step);
-	void Draw_Level(HDC hdc);
 	void Draw_Platform(HDC hdc, int x, int y);
 	void Draw_Border(HDC hdc, int x, int y, bool top_boder);
 	void Draw_Bounds(HDC hdc, RECT &paint_area);
 
-	HPEN Highlight_Pen, Letter_Pen, Brick_Red_Pen, Brick_Blue_Pen, Platform_Circle_Pen, Platform_Inner_Pen, Border_Blue_Pen, Border_White_Pen;
-	HBRUSH Brick_Red_Brush, Brick_Blue_Brush, Platform_Circle_Brush, Platform_Inner_Brush, Border_Blue_Brush, Border_White_Brush;
+	HPEN Highlight_Pen, Platform_Circle_Pen, Platform_Inner_Pen, Border_Blue_Pen, Border_White_Pen;
+	HBRUSH Platform_Circle_Brush, Platform_Inner_Brush, Border_Blue_Brush, Border_White_Brush;
 
 	int Inner_Width;
 	int Platform_X_Step;
 
 	RECT Platform_Rect, Prev_Platform_Rect;
-	RECT Level_Rect;
 
 	ABall Ball;
+	ALevel Level;
 
-	static const int Brick_Width = 15;
-	static const int Brick_Height = 7;
 	static const int Circle_Size = 7;
 	static const int Platform_Height = 7;
 };
