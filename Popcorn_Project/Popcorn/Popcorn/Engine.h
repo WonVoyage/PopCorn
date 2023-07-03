f9c7c933879c38cb6668db6f2c931a0627911cec
WonVoyage <137709423+WonVoyage@users.noreply.github.com>
2023-07-03 18:52:40 +0300

    Move platform to platform class

--- Popcorn_Project/Popcorn/Popcorn/Engine.h
+++ Popcorn_Project/Popcorn/Popcorn/Engine.h
@@ -31,13 +31,16 @@ const int Timer_ID = WM_USER + 1;
 //------------------------------------------------------------------------------------------------------------
 class AsEngine;
 class ALevel;
+class AsPlatform;
 class ABall
 {
 public:
 	ABall();
 
+	void Init();
+
 	void Draw(HDC hdc, RECT &paint_area, AsEngine *engine);
-	void Move(AsEngine *engine, ALevel *level);
+	void Move(AsEngine *engine, ALevel *level, AsPlatform *platform);
 
 	HPEN Ball_Pen;
 	HBRUSH Ball_Brush;
@@ -79,6 +82,33 @@ private:
 	static const int Brick_Height = 7;
 };
 //------------------------------------------------------------------------------------------------------------
+class AsPlatform
+{
+public:
+	AsPlatform();
+
+	void Init();
+	void Redraw_Platform(AsEngine *engine);
+	void Draw_Platform(HDC hdc, AsEngine *engine, RECT &paint_area);
+
+	int Platform_X_Pos;
+	int Platform_Width;
+	int Platform_X_Step;
+
+	static const int Platform_Y_Pos = 185;
+
+private:
+	int Inner_Width;
+
+	RECT Platform_Rect, Prev_Platform_Rect;
+
+	HPEN Highlight_Pen, Platform_Circle_Pen, Platform_Inner_Pen;
+	HBRUSH Platform_Circle_Brush, Platform_Inner_Brush;
+
+	static const int Platform_Height = 7;
+	static const int Circle_Size = 7;
+};
+//------------------------------------------------------------------------------------------------------------
 class AsEngine
 {
 public:
@@ -94,34 +124,22 @@ public:
 	HWND Hwnd;
 	HPEN BG_Pen;
 	HBRUSH BG_Brush;
-	int Platform_X_Pos;
-	int Platform_Width;
 
 	static const int Global_Scale = 3;
 	static const int Max_X_Pos = ALevel::Level_X_Offset + ALevel::Cell_Width * ALevel::Level_Width;
 	static const int Max_Y_Pos = 199 - ABall::Ball_Size;
-	static const int Platform_Y_Pos = 185;
 	static const int Border_X_Offset = 6;
 	static const int Border_Y_Offset = 4;
 
 private:
-	void Redraw_Platform();
-	void Draw_Platform(HDC hdc, int x, int y);
 	void Draw_Border(HDC hdc, int x, int y, bool top_boder);
 	void Draw_Bounds(HDC hdc, RECT &paint_area);
 
-	HPEN Highlight_Pen, Platform_Circle_Pen, Platform_Inner_Pen, Border_Blue_Pen, Border_White_Pen;
-	HBRUSH Platform_Circle_Brush, Platform_Inner_Brush, Border_Blue_Brush, Border_White_Brush;
-
-	int Inner_Width;
-	int Platform_X_Step;
-
-	RECT Platform_Rect, Prev_Platform_Rect;
+	HPEN Border_Blue_Pen, Border_White_Pen;
+	HBRUSH Border_Blue_Brush, Border_White_Brush;
 
 	ABall Ball;
 	ALevel Level;
-
-	static const int Circle_Size = 7;
-	static const int Platform_Height = 7;
+	AsPlatform Platform;
 };
 //------------------------------------------------------------------------------------------------------------
