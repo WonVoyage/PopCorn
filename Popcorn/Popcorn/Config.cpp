d33680bfd2b38ba8f76726280ff6d5fa44819a19
WonVoyage <WonVoyage@yandex.ru>
2023-07-16 20:49:00 +0300

    Revert "Merge pull request #21 from WonVoyage/Platform-meltdown"
    This reverts commit d46c8fb6eb6c1d67e59be621dbf220aa8c79dad2, reversing
    changes made to 7f5a95dab29d3ba8a8b4adac73a953d95de0edf3.

--- Popcorn/Popcorn/Config.cpp
+++ Popcorn/Popcorn/Config.cpp
@@ -12,8 +12,6 @@ AColor::AColor(unsigned char r, unsigned char g, unsigned char b)
 
 
 // AsConfig
-int AsConfig::Current_Timer_Tick = 0;
-
 const AColor AsConfig::BG_Color(15, 63, 31);
 const AColor AsConfig::Red_Brick_Color(255, 85, 85);
 const AColor AsConfig::Blue_Brick_Color(85, 255, 255);
@@ -36,11 +34,5 @@ void AsConfig::Create_Pen_Brush(unsigned char r, unsigned char g, unsigned char 
 {
 	pen = CreatePen(PS_SOLID, 0, RGB(r, g, b));
 	brush = CreateSolidBrush(RGB(r, g, b));
-}
-//------------------------------------------------------------------------------------------------------------
-int AsConfig::Rand(int range)
-{// Вычисляет псевдослучайное число в диапазоне [0, .. range - 1]
-
-	return rand() * range / RAND_MAX;
 }
 //------------------------------------------------------------------------------------------------------------
--- Popcorn/Popcorn/Platform.cpp
+++ Popcorn/Popcorn/Platform.cpp
@@ -3,11 +3,9 @@
 // AsPlatform
 //------------------------------------------------------------------------------------------------------------
 AsPlatform::AsPlatform()
-: X_Pos(AsConfig::Border_X_Offset), X_Step(AsConfig::Global_Scale * 2), Platform_State(EPS_Normal), Inner_Width(21),
-  Width(Normal_Width), Platform_Rect{}, Prev_Platform_Rect{}, Highlight_Pen(0), Platform_Circle_Pen(0),
-  Platform_Inner_Pen(0), Platform_Circle_Brush(0), Platform_Inner_Brush(0)
+	: Inner_Width(21), X_Pos(AsConfig::Border_X_Offset), X_Step(AsConfig::Global_Scale * 2), Width(28), Platform_Rect{},
+	Prev_Platform_Rect{}, Highlight_Pen(0), Platform_Circle_Pen(0), Platform_Inner_Pen(0), Platform_Circle_Brush(0), Platform_Inner_Brush(0)
 {
-	X_Pos = (AsConfig::Max_X_Pos - Width) / 2;
 }
 //------------------------------------------------------------------------------------------------------------
 void AsPlatform::Init()
@@ -16,24 +14,6 @@ void AsPlatform::Init()
 
 	AsConfig::Create_Pen_Brush(151, 0, 0, Platform_Circle_Pen, Platform_Circle_Brush);
 	AsConfig::Create_Pen_Brush(0, 128, 192, Platform_Inner_Pen, Platform_Inner_Brush);
-}
-//------------------------------------------------------------------------------------------------------------
-void AsPlatform::Act(HWND hwnd)
-{
-	int i, len;
-
-	if (Platform_State != EPS_Meltdown)
-	{
-		Platform_State = EPS_Meltdown;
-
-		len = sizeof(Meltdown_Platform_Y_Pos) / sizeof(Meltdown_Platform_Y_Pos[0]);
-
-		for (i = 0; i < len; i++)
-			Meltdown_Platform_Y_Pos[i] = Platform_Rect.bottom;
-	}
-
-	if (Platform_State == EPS_Meltdown)
-		Redraw_Platform(hwnd);
 }
 //------------------------------------------------------------------------------------------------------------
 void AsPlatform::Redraw_Platform(HWND hwnd)
@@ -45,30 +25,12 @@ void AsPlatform::Redraw_Platform(HWND hwnd)
 	Platform_Rect.right = Platform_Rect.left + Width * AsConfig::Global_Scale;
 	Platform_Rect.bottom = Platform_Rect.top + Height * AsConfig::Global_Scale;
 
-	if (Platform_State == EPS_Meltdown)
-		Prev_Platform_Rect.bottom = (AsConfig::Max_Y_Pos + 1) * AsConfig::Global_Scale;
-
 	InvalidateRect(hwnd, &Prev_Platform_Rect, FALSE);
 	InvalidateRect(hwnd, &Platform_Rect, FALSE);
 }
 //------------------------------------------------------------------------------------------------------------
 void AsPlatform::Draw(HDC hdc, RECT &paint_area)
 {// Рисуем платформу
-
-	switch (Platform_State)
-	{
-	case EPS_Normal:
-		Draw_Normal_State(hdc, paint_area);
-		break;
-
-	case EPS_Meltdown:
-		Draw_Meltdown_State(hdc, paint_area);
-		break;
-	}
-}
-//------------------------------------------------------------------------------------------------------------
-void AsPlatform::Draw_Normal_State(HDC hdc, RECT &paint_area)
-{// Рисуем платформу в нормальном состоянии
 
 	int x = X_Pos;
 	int y = AsConfig::Platform_Y_Pos;
@@ -100,45 +62,5 @@ void AsPlatform::Draw_Normal_State(HDC hdc, RECT &paint_area)
 	SelectObject(hdc, Platform_Inner_Brush);
 
 	RoundRect(hdc, (x + 4) * AsConfig::Global_Scale, (y + 1) * AsConfig::Global_Scale, (x + 4 + Inner_Width - 1) * AsConfig::Global_Scale, (y + 1 + 5) * AsConfig::Global_Scale, 3 * AsConfig::Global_Scale, 3 * AsConfig::Global_Scale);
-}
-//------------------------------------------------------------------------------------------------------------
-void AsPlatform::Draw_Meltdown_State(HDC hdc, RECT &paint_area)
-{// Рисуем платформу в расплавленном состоянии
-
-	int i, j;
-	int x, y;
-	int y_offset;
-	int area_width, area_height;
-	COLORREF pixel;
-	COLORREF bg_pixel = RGB(AsConfig::BG_Color.R, AsConfig::BG_Color.G, AsConfig::BG_Color.B);
-	RECT intersection_rect;
-
-	if (! IntersectRect(&intersection_rect, &paint_area, &Platform_Rect) )
-		return;
-
-	area_width = Width * AsConfig::Global_Scale;
-	area_height = Height * AsConfig::Global_Scale + 1;
-
-	for (i = 0; i < area_width; i++)
-	{
-		y_offset = AsConfig::Rand(Meltdown_Speed) + 1;
-		x = Platform_Rect.left + i;
-
-		for (j = 0; j < area_height; j++)
-		{
-			y = Meltdown_Platform_Y_Pos[i] - j;
-
-			pixel = GetPixel(hdc, x, y);
-			SetPixel(hdc, x, y + y_offset, pixel);
-		}
-
-		for (j = 0; j < y_offset; j++)
-		{
-			y = Meltdown_Platform_Y_Pos[i] - area_height + 1 + j;
-			SetPixel(hdc, x, y, bg_pixel);
-		}
-
-		Meltdown_Platform_Y_Pos[i] += y_offset;
-	}
 }
 //------------------------------------------------------------------------------------------------------------
