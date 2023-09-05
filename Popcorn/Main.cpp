dad5ae1fa3831890d0c0db6b7b2389e8b6011794
WonVoyage <WonVoyage@yandex.ru>
2023-09-05 13:05:19 +0300

    Unbreakable brick
    * Add animation code to AActive_Brick::Act() method
    * Add AActive_Brick_Unbreakable class
    * Move the general functionality to the AActive_Brick class
    * Move the AActive_Brick constructor to the protected one
    * Add virtual destructor AGraphics_Object
    * Add unbreakable brick to Add_Active_Brick()
    * Add drawing an unbreakable brick in Draw_Brick() method

--- Popcorn/Main.cpp
+++ Popcorn/Main.cpp
@@ -18,7 +18,7 @@ BOOL InitInstance(HINSTANCE, int);
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK About(HWND, UINT, WPARAM, LPARAM);
 //------------------------------------------------------------------------------------------------------------
-int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
 {
 	UNREFERENCED_PARAMETER(hPrevInstance);
 	UNREFERENCED_PARAMETER(lpCmdLine);
